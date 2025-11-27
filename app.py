107edc9df1328ca2a0479c124ced01a3b6dcc07d
qusakabi <sqwrtick1@gmail.com>
2025-11-27 15:00:55 +0300

    fix app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def hello():
     return "Hello from AWS Elastic Beanstalk via GitHub Actions! 🚀"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    application.run(host='0.0.0.0', port=5000)
